f53128269ab493cb0beb5a59cf8d52720b43a392
kurumi0512 <foreverlove0512t@gmail.com>
2025-06-05 09:27:11 +0800

    250605 09:27

---
+++ src/main/java/com/example/demo/McpClient8080Application.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.mcp.SyncMcpToolCallbackProvider;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class McpClient8080Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpClient8080Application.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner runner(ChatClient.Builder chatClientBuilder,
+			SyncMcpToolCallbackProvider toolCallbackProvider) {
+		return args -> {
+			// 建立 ChatClient 並將 callback provider 註冊進去
+			ChatClient chatClient = chatClientBuilder.defaultTools(toolCallbackProvider).build();
+
+			String prompt1 = "今天似乎運氣不錯想買5組樂透彩券並顯示這5組號碼, 並幫我結帳";
+			// String prompt1 = "今天似乎運氣不錯想買5組樂透彩券並顯示這5組號碼, 不要幫我結帳";
+			System.out.println(prompt1);
+
+			String response1 = chatClient.prompt().user(prompt1).call().content();
+
+			System.out.println("AI Agent 回應:" + response1);
+		};
+	}
+}
